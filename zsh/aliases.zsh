c20b6cba4fa02778dc28b860fe39a55fba841571
Jules V <jules.vitrac@epitech.eu>
2022-07-13 18:27:54 +0200

    Scripted auto-commit on change (2022-07-13 18:27:54) by gitwatch.sh

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -8,3 +8,7 @@ alias gsl="git shortlog"
 alias icat="kitty +kitten icat"
 alias f="fuck"
 alias hgrep="history | grep"
+alias clear="printf '\033[2J\033[3J\033[1;1H'"
+send() {
+    scp -P 20023 $1 HyNeko@notkani.me:/Users/HyNeko/sharedfiles/$2
+}
