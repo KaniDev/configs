1e911f1ec2bd00c676b4705afc2d7f69d06dd6af
Jules V <jules.vitrac@epitech.eu>
2024-03-04 18:44:11 +0100

    Autoupdate

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -8,7 +8,9 @@ alias icat="kitty +kitten icat"
 alias f="fuck"
 alias hgrep="history | grep"
 alias clear="printf '\033[2J\033[3J\033[1;1H'"
-alias ssh="kitty +kitten ssh"
+#alias ssh="kitty +kitten ssh"
 send() {
     scp -P 20023 $1 HyNeko@notkani.me:/Users/HyNeko/sharedfiles/$2
 }
+alias hibernate="sudo systemctl hibernate"
+alias slippi="/usr/lib/slippi-online/slippi-online -u ~/.config/SlippiOnline/"
